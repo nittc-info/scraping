f6bd3634981f0ebee011850bacc654cc2367feab
arata <arata04962@gmail.com>
2020-01-09 17:19:05 +0900

    :+1: Add 'debug' command-line argument

--- tnct-info/main.py
+++ tnct-info/main.py
@@ -1,12 +1,24 @@
-import db
-import logger
+import argparse
+from libs import logger, db
 from extractor.classextractor import ClassExtractor
+def main():
+    is_debug = is_debugging()
+    update_class(is_debug)
+    update_event(is_debug)
 
 
-def update_class():
+def is_debugging() -> bool:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--debug", action="store_true")
+    args = parser.parse_args()
+    return args.debug
+
+
+def update_class(is_debug: bool):
     ce = ClassExtractor()
     class_infos = ce.extract_all()
-    [add_class_to_db('classes', ci) for ci in class_infos]
+    if not is_debug:
+        [add_class_to_db('classes', ci) for ci in class_infos]
     logger.info(f'{len(class_infos)} class changes updated.')
 
 
@@ -14,10 +26,6 @@ def add_class_to_db(col, ci):
     if not db.document_exists(col, ci.to_id()):
         db.set_value(col, ci.to_id(), ci.to_dict())
         logger.info(f'Added :{ci}')
-
-
-def main():
-    update_class()
 
 
 if __name__ == '__main__':
